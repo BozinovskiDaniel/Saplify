6c49d478c1e5c0c05005e47feb9cc53af7819b48
BozinovskiDaniel <BozinovskiDaniel@hotmail.com>
2020-04-18 23:19:01 +1000

    almost got the signup functionality working

--- functions/index.js
+++ functions/index.js
@@ -7,10 +7,24 @@ var serviceAccount = require("./saplify-48591-87f9ad72d7c4.json");
 const express = require('express');
 const app = express();
 
+var config = {
+    apiKey: "AIzaSyA1avcmxY0Z8vLNWTRQkeuEY5LTG4Xd3oA",
+    authDomain: "saplify-48591.firebaseapp.com",
+    databaseURL: "https://saplify-48591.firebaseio.com",
+    projectId: "saplify-48591",
+    storageBucket: "saplify-48591.appspot.com",
+    messagingSenderId: "427045407305",
+    appId: "1:427045407305:web:aa7b95c22321842c5b4d98",
+    measurementId: "G-MEBH7PFJ58"
+  };
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://saplify-48591.firebaseio.com"
 });
+
+const firebase = require('firebase');
+firebase.initializeApp(config)
 
 app.get('/screams', (req, res) => {
 
@@ -52,4 +66,49 @@ app.post('/scream', (req, res) => {
 
 });
 
-exports.api = functions.region('asia-east2').https.onRequest(app);
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle,
+    }
+
+    let token, userId;
+    admin.firestore().doc(`/users/${newUser.handle}`).get()
+    .then(doc => {
+        if (doc.exists) {
+            return res.status(400).json({ handle:' this handle is already taken' })
+        } else {
+            return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+        }
+    })
+    .then((data) => {
+        userId = data.user.uid;
+        return data.user.getIdToken();
+    })
+    .then((idToken) => {
+        token = idToken;
+        const userCredentials = {
+            handle: newUser.handle,
+            email: newUser.email,
+            createdAt: new Date().toISOString(),
+            userId
+        };
+        return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+        return res.status(201).json({ token });
+    })
+    .catch(err => {
+        console.error(err);
+        if (err.code === "auth/email-already-in-use") {
+            return res.status(400).json({ email: "Email is already in use"});
+        } else {
+            return res.status(500).json({ error: err.code });
+        }
+    })
+    
+});
+
+exports.api = functions.https.onRequest(app);
