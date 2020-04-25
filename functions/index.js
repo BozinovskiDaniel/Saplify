f9ea3117a131294be7f857a9561c8080a0f6b7aa
BozinovskiDaniel <BozinovskiDaniel@hotmail.com>
2020-04-25 21:27:19 +1000

    finished with most of the backend, implemented basic start for react frontend

--- functions/index.js
+++ functions/index.js
@@ -27,7 +27,6 @@ const {
 app.get("/screams", getAllScreams);
 app.post("/scream", FBAuth, postOneScream);
 app.get("/scream/:screamId", getScream);
-
 app.delete("/scream/:screamId", FBAuth, deleteScream);
 app.get("/scream/:screamId/like", FBAuth, likeScream);
 app.get("/scream/:screamId/unlike", FBAuth, unlikeScream);
@@ -44,6 +43,7 @@ app.post("/notifications", FBAuth, markNotificationsRead);
 
 exports.api = functions.https.onRequest(app);
 
+// Event handlers
 exports.createNotificationOnLike = functions.firestore
   .document("likes/{id}")
   .onCreate((snapshot) => {
